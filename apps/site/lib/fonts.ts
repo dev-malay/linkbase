69c6716a4ee0cdf70518c472155dd55b175e9fa7
dev-malay <malayworkz@gmail.com>
2026-05-17 22:32:09 +0530

    ft. add utils

---
+++ apps/site/lib/fonts.ts
@@ -0,0 +1,8 @@
+import { Inclusive_Sans } from "next/font/google";
+
+export const inclusiveSans = Inclusive_Sans({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-inclusive-sans",
+  display: "swap",
+});
